96eddcc9160851b1ba4ee6b73b1039d6bc6b5ec4
cristian <cristian.fonolla@gmail.com>
2017-04-04 16:56:39 +0200

    fixed something wrong

--- resources/assets/js/components/wizardStep.vue
+++ resources/assets/js/components/wizardStep.vue
@@ -1,39 +1,32 @@
 <template>
-
-    <div role="tabpanel" class="tab-pane">
-
-
+    <div role="tabpanel" class="tab-pane" :class="{'active': active}" :id="id">
+        <slot></slot>
     </div>
-
-
 </template>
 
 <script>
-    import voca from 'voca';
+    import voca from 'voca'
     export default {
-
-        props: {
-
-            title: {
+        props:{
+            title:{
                 type: String,
                 required: true
             },
-            active: {
+            active:{
                 type:[Boolean,String],
                 default:false
             }
         },
-        computed: {
-            id: function () {
-                return voca.latinise(voca.camelcase(this.title))
+        computed : {
+            id: function(){
+                return voca.latinise(voca.camelCase(this.title))
             },
-            link: function () {
+            link: function(){
                 return '#' + this.id
             }
-         },
+        },
         mounted() {
-            console.log('wizard-step done')
+            console.log('Component mounted.')
         }
     }
-
 </script>
